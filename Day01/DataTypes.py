e794226e079851c39138e8d047b516641eb6537c
Bijay Jha <bijayjha12345@gmail.com>
2026-01-12 23:18:12 +0545

    Basic data types and type function

---
+++ Day01/DataTypes.py
@@ -0,0 +1,19 @@
+a = 10     
+print(type(a))    #gives <class 'int'>
+
+b= "Bijay"
+print(type(b))   #gives <class 'str'>
+
+c = 3.567
+print(type(c))   #gives <class 'float'>
+
+d = True
+print(type(d))   #gives <class 'bool'>
+
+e = None
+print(type(e))   #gives <class 'NoneType'>
+
+z = 5 + 3j
+print(type(z))   #gives <class 'complex'>
+
+#The above are the basic data types in Python while type() function is used to know the data type of a variable.
