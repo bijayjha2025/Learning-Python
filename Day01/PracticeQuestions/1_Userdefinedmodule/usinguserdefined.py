b86c45af5fcb783a10e438ddc1588e01b214078c
Bijay Jha <bijayjha12345@gmail.com>
2026-01-13 13:37:32 +0545

    Practicing a basic user-defined module

---
+++ Day01/PracticeQuestions/1_Userdefinedmodule/usinguserdefined.py
@@ -0,0 +1,7 @@
+
+import calc
+print("Addition:", calc.add(10, 5))
+print("Subtraction:", calc.subtract(10, 5))
+print("Brand Name:", calc.brandName)
+
+#The above code demonstrates how to use a user-defined module named 'calc' which contains functions for addition and subtraction, as well as a variable for brand name. first, we import the module using the 'import' statement, then we call the functions and access the variable using the module name as a prefix. Note that the module file 'calc.py' should be in the same directory as this script or in the Python path for the import to work correctly as well as the function and variable names should match those defined in the module. Also, parameters must be same as defined in the module.
