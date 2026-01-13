47753922184fc3003a052a0aea0803ab51c8f5c8
Bijay Jha <bijayjha12345@gmail.com>
2026-01-13 22:18:43 +0545

    Practing id question

---
+++ Day01/PracticeQuestions/id.py
@@ -0,0 +1,11 @@
+
+a = 500
+b = 200
+
+print(f"a:{a}, Id: {id(a)}")
+print(f"b:{b}, Id: {id(b)}")
+
+print(f"Are they same objects? {a is b}")
+
+# Here, they are not the same objects as their ids are different.
+
